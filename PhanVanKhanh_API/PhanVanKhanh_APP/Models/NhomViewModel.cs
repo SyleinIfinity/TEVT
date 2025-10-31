433563762aa02dd56dee754e3eef0a36a34c110d
SyleinIfinity <khanhsky2k5nam@gmail.com>
2025-10-31 09:23:23 +0700

    Delete PhanVanKhanh_API directory

--- PhanVanKhanh_API/PhanVanKhanh_APP/Models/NhomViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PhanVanKhanh_APP.Models // Namespace đúng
-{
-    public class NhomViewModel
-    {
-        public int MaNhom { get; set; } // Khớp với API Model
-        public string TenNhom { get; set; } // Khớp với API Model
-    }
-}
